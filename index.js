25d5e62793b1e20129bddbaa8a82a3b60009ad04
siddhxrthh <ksiddharth127@gmail.com>
2021-05-22 17:06:08 +0530

    corsOptions

--- index.js
+++ index.js
@@ -5,16 +5,20 @@ const cors = require('cors');
 const bodyParser = require("body-parser");
 const port = process.env.PORT;
 app.use(bodyParser.json());
-app.use(cors());
+const corsOptions = {
+  credentials: true,
+  origin: true,
+};
+app.use(cors(corsOptions));
 app.use(bodyParser.urlencoded({ extended: false }));
 app.use(express.static('./public'));
 const http = require("http");
 const routes = require("./api/routes");
-app.use(function (req, res, next) {
-  res.header("Access-Control-Allow-Origin", "*");
-  res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
-  next();
-});
+// app.use(function (req, res, next) {
+//   res.header("Access-Control-Allow-Origin", "*");
+//   res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
+//   next();
+// });
 
 app.use("/api", routes);
 
