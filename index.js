15bc2cf256ed8873f99f9631809bdbe808d099a3
siddhxrthh <ksiddharth127@gmail.com>
2021-05-22 16:44:49 +0530

    access control

--- index.js
+++ index.js
@@ -10,6 +10,12 @@ app.use(bodyParser.urlencoded({ extended: false }));
 app.use(express.static('./public'));
 const http = require("http");
 const routes = require("./api/routes");
+app.use(function (req, res, next) {
+  res.header("Access-Control-Allow-Origin", "*");
+  res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
+  next();
+});
+
 app.use("/api", routes);
 
 const server = http.createServer(app);
